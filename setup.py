30b8911d359695c6e2680cc2b97b2c101cfa5a7f
Bernard Salanie <bs2237@columbia.edu>
2023-02-16 18:47:32 -0300

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bsalanie/cupid_matching',
-    version='0.2.3',
+    version='0.3.0',
     zip_safe=False,
 )
